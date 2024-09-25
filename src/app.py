96e3de7c0ecd70765d02d3e16b2b8d054b8f4462
msyamsul725 <msyamsul7279@gmail.com>
2024-09-25 23:36:48 +0700

    -

--- src/app.py
+++ src/app.py
@@ -5,7 +5,9 @@ import cv2
 import numpy as np
 from scipy.spatial import ConvexHull
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
 # Path to the shape predictor model
 shape_predictor_path = './shape_predictor_68_face_landmarks.dat'
 def load_image_and_encode(image_data):
@@ -97,4 +99,4 @@ def recognize_face():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
